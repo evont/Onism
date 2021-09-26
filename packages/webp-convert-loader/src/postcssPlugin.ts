aab6b60febc94cc17c0e0930bedb5873947bc4ea
EvontNg <evontng@EvontNg.local>
2021-09-26 15:50:22 +0800

    feat(webp-convert): support convert to webp if size is smaller
    affects: @onism/webp-convert-loader

--- packages/webp-convert-loader/src/postcssPlugin.ts
+++ packages/webp-convert-loader/src/postcssPlugin.ts
@@ -1,9 +1,9 @@
-import { Declaration, Node, PluginCreator, Rule } from "postcss";
+import { Declaration, PluginCreator, Rule } from "postcss";
 import valueParser, { FunctionNode } from "postcss-value-parser";
+import { getHashDigest } from "loader-utils";
 import { transformAlias, startsWith } from "./util";
 import * as path from "path";
 import * as fs from "fs-extra";
-import { ImagePool } from "@squoosh/lib";
 import sharp from "sharp";
 const targets = {
   ".png": "oxipng",
@@ -15,83 +15,57 @@ const targets = {
   // ...minifyOptions.targets,
 };
 
-// async function minify({
-//   loaderContext,
-//   url
-// }) {
-  
-//   const _compilation = loaderContext._compilation;
-//   try {
-//     const imagePath = await new Promise<string>((resolve, reject) =>
-//       loaderContext.resolve(loaderContext.context, url, (err, result) =>
-//         err ? reject(err) : resolve(result)
-//       )
-//     );
-//     const ext = path.extname(imagePath).toLowerCase();
-//     const targetCodec = targets[ext];
-//     if (!targetCodec) {
-//       throw new Error(
-//         `The "${imagePath}" was not minified, ${ext} extension is not supported".`
-//       );
-//     }
-//     const encodeOptions = {
-//       [targetCodec]: {},
-//       webp: {},
-//       // ...minifyOptions.encodeOptions,
-//     };
-//     const len = cpus().length;
-//     const imagePool = new ImagePool(len);
-//     const image = imagePool.ingestImage(imagePath);
-//     await image.decoded;
-//     await image.preprocess({
-//       quant: {
-//         numColors: 256,
-//         dither: 0.5,
-//       },
-//     });
-//     await image.encode(encodeOptions);
-//     await imagePool.close();
-//     //TODO: compare image size
-//     const rawImage = await image.encodedWith[targetCodec];
-//     const rawImageInWebp = await image.encodedWith.webp;
-//     // fs.outputFile(imagePath, rawImage.binary);
-//     // console.log(rawImageInWebp.size, rawImage.size)
-//     // if (rawImageInWebp.size < rawImage.size) {
-//     //   fs.outputFileSync(imagePath + ".webp", rawImageInWebp.binary);
-//     //   result[selector].url.push(url);
-//     // } else {
-//     //   console.log(imagePath);
-//     // }
+async function convertToWebp(url, loaderContext) {
+  try {
+    const imagePath = await new Promise<string>((resolve, reject) =>
+      loaderContext.resolve(loaderContext.context, url, (err, result) =>
+        err ? reject(err) : resolve(result)
+      )
+    );
+    const old = await sharp(imagePath);
+    const { info: { size }, data: oldData, hash } = await new Promise((resolve, reject) => {
+      old.toBuffer((err, data, info) => {
+        if (err) reject(err);
+        const hash = getHashDigest(data);
+        resolve({
+          data,
+          info,
+          hash
+        })
+      });
+    })
     
-//     const { path: newName } = _compilation.getPathWithInfo(
-//       "[path][name][ext].webp",
-//       {
-//         filename: imagePath,
-//       }
-//     );
-//     const minifyPath = path.resolve(
-//       path.dirname(imagePath),
-//       "minify",
-//       path.basename(imagePath)
-//     );
-//     loaderContext.emitFile(
-//       path.basename(imagePath),
-//       Buffer.from(rawImage.binary),
-//       ""
-//     );
-//     if (rawImageInWebp.size < rawImage.size) {
-//       loaderContext.emitFile(
-//         path.basename(imagePath) + ".webp",
-//         Buffer.from(rawImageInWebp.binary),
-//         ""
-//       );
-//     }
+    const webp = await old.webp({
+      lossless: true
+    })
 
-//   } catch (err) {
-//     console.error(err);
-//     console.error(`${url} is not found`);
-//   }
-// }
+    const { info: { size: webpSize }, data: webpData, hash: webpHash } = await new Promise((resolve, reject) => {
+      webp.toBuffer((err, data, info) => {
+        if (err) reject(err);
+        const hash = getHashDigest(data);
+        resolve({
+          data,
+          info,
+          hash
+        })
+      });
+    })
+
+    if (webpSize < size) {
+      const imgName = `${imagePath}.webp`;
+      await webp.toFile(imgName);
+      return {
+        hash,
+        webpHash,
+        imgName
+      }
+    }
+    return null;
+  } catch (err) {
+    console.error(err);
+    console.error(`${url} is not found`);
+  }
+}
 export default ({ loaderContext, options }) => {
   let { outputPath, className } = options;
   const compilerOptions = loaderContext._compiler.options;
@@ -119,134 +93,92 @@ export default ({ loaderContext, options }) => {
     realOutput = path.resolve(_context, realOutput);
     outputPath = path.resolve(_context, outputPath);
   }
-  const result: Record<
-    string,
-    {
-      url: string[];
-      decl: Declaration;
-      webpDecl: Declaration;
-    }
-  > = {};
   const DEFAULT_OPTIONS = {
     modules: false,
-    noWebpClass: 'no-webp',
-    webpClass: 'webp',
+    noWebpClass: "no-webp",
+    webpClass: "webp",
     addNoJs: true,
-    noJsClass: 'no-js',
-    rename: oldName => {
-      return oldName.replace(/\.(jpe?g|png)/gi, '.$1.webp')
-    }
-  }
-  let { modules, noWebpClass, webpClass, addNoJs, noJsClass, rename } = {
+    noJsClass: "no-js"
+  };
+  let { modules, noWebpClass, webpClass, addNoJs, noJsClass } = {
     ...DEFAULT_OPTIONS,
-  }
+  };
   function removeHtmlPrefix(className) {
-    return className.replace(/html ?\./, '')
+    return className.replace(/html ?\./, "");
   }
   function addClass(selector, className) {
-    let generatedNoJsClass
-    let initialClassName = className
-    if (className.includes('html')) {
-      className = removeHtmlPrefix(className)
+    let generatedNoJsClass;
+    let initialClassName = className;
+    if (className.includes("html")) {
+      className = removeHtmlPrefix(className);
     }
     if (modules) {
-      className = `:global(.${className})`
-      generatedNoJsClass = `:global(.${noJsClass})`
+      className = `:global(.${className})`;
+      generatedNoJsClass = `:global(.${noJsClass})`;
     } else {
-      className = `.${className}`
-      generatedNoJsClass = `.${noJsClass}`
+      className = `.${className}`;
+      generatedNoJsClass = `.${noJsClass}`;
     }
-    if (selector.includes('html')) {
-      selector = selector.replace(/html[^ ]*/, `$& body${className}`)
+    if (selector.includes("html")) {
+      selector = selector.replace(/html[^ ]*/, `$& body${className}`);
     } else {
-      selector = `body${className} ` + selector
+      selector = `body${className} ` + selector;
     }
     if (addNoJs && initialClassName === noWebpClass) {
       selector +=
-        ', ' +
-        selector.split(`body${className}`).join(`body${generatedNoJsClass}`)
+        ", " +
+        selector.split(`body${className}`).join(`body${generatedNoJsClass}`);
     }
-    return selector
+    return selector;
   }
   fs.ensureDirSync(realOutput);
+  const cacheMap = new Map();
   const PostcssPlugin: PluginCreator<{}> = function () {
     return {
       postcssPlugin: "webp-connvert-parser",
       async Declaration(decl) {
         if (/\.(jpe?g|png)(?!(\.webp|.*[&?]format=webp))/i.test(decl.value)) {
           let rule = decl.parent as Rule;
-          if (rule.selector.includes(`.${removeHtmlPrefix(noWebpClass)}`) || rule.selector.includes(`.${removeHtmlPrefix(noJsClass)}`)) return
-          let webp = rule.clone();
-          webp.each((i: Declaration) => {
-            if (i.prop !== decl.prop && i.value !== decl.value && !/WEBP_CONVER__/.test(i.prop)) i.remove()
-          })
-          webp.selectors = webp.selectors.map(i => addClass(i, webpClass));
-          webp.each((i: Declaration) => {
-            if (
-              rename &&
-              Object.prototype.toString.call(rename) === '[object Function]'
-            ) {
-              i.value = rename(i.value)
-              if (!/WEBP_CONVER__/.test(i.prop)) i.prop = "WEBP_CONVER__" + i.prop;
-              
-            }
-            console.log(i.prop, i.value);
-          })
-          let noWebp = rule.cloneAfter()
+          if (
+            rule.selector.includes(`.${removeHtmlPrefix(noWebpClass)}`) ||
+            rule.selector.includes(`.${removeHtmlPrefix(noJsClass)}`) ||
+            rule.selector.includes(`.${removeHtmlPrefix(webpClass)}`)
+          ) {
+            return;
+          }
+
+          let noWebp = rule.cloneAfter();
           noWebp.each((i: Declaration) => {
-            if (i.prop !== decl.prop && i.value !== decl.value) i.remove()
-          })
-          noWebp.selectors = noWebp.selectors.map(i => addClass(i, noWebpClass))
-          decl.remove()
-          if (rule.nodes.length === 0) rule.remove()
+            if (i.prop !== decl.prop && i.value !== decl.value) i.remove();
+          });
+          noWebp.selectors = noWebp.selectors.map((i) =>
+            addClass(i, noWebpClass)
+          );
+
+          let webp = rule.cloneAfter();
+          webp.each((i: Declaration) => {
+            if (i.prop !== decl.prop && i.value !== decl.value) i.remove();
+          });
+          webp.selectors = webp.selectors.map((i) => addClass(i, webpClass));
+          for (const node of webp.nodes) {
+            const i = node as Declaration;
+              const { nodes } = valueParser(decl.value);
+              for (const node of nodes) {
+                if (node.value === "url") {
+                  const [urlNode] = (node as FunctionNode).nodes;
+                  const url = urlNode.value;
+                  const converResult = await convertToWebp(url, loaderContext);
+                  if (converResult) {
+                    const { imgName } = converResult;
+                    i.value = i.value.replace(url, imgName)
+                  }
+                }
+              }
+          }
+          decl.remove();
+          if (rule.nodes.length === 0) rule.remove();
         }
-        // if (decl.prop === "background" || decl.prop === "background-image") {
-        //   const { selector } = decl.parent as Rule;
-        //   result[selector] = Object.assign(result[selector] || {}, {
-        //     url: result[selector]?.url || [],
-        //     decl,
-        //     webpDecl: undefined,
-        //   });
-        //   const { nodes } = valueParser(decl.value);
-        //   for (const node of nodes) {
-        //     if (node.value === "url") {
-        //       const [urlNode] = (node as FunctionNode).nodes;
-        //       const url = urlNode.value;
-        //       result[selector].url.push(url);
-        //     }
-        //   }
-        // }
       },
-      // async OnceExit(root, { Rule }) {
-      //   //console.log(root)
-      //   const nowebps = [];
-      //   const webps = [];
-      //   for (const item in result) {
-      //     for (const url of result[item].url) {
-      //       console.log(url);
-      //       await minify({
-      //         loaderContext,
-      //         url
-      //       })
-      //     }
-      //     // const norule = new Rule({ selector: `.${className.nowebp} ${item}` });
-      //     // norule.append(result[item].decl);
-      //     // nowebps.push(norule);
-
-      //     // let finalValue = result[item].decl.value;
-      //     // result[item].url.forEach((url) => {
-      //     //   finalValue = finalValue.replace(url, url + ".webp");
-      //     // });
-      //     // result[item].webpDecl = result[item].decl.clone({
-      //     //   value: finalValue,
-      //     // });
-
-      //     // const rule = new Rule({ selector: `.${className.webp} ${item}` });
-      //     // rule.append(result[item].webpDecl);
-      //     // webps.push(rule);
-      //   }
-      //   root.append([...nowebps, ...webps]);
-      // },
     };
   };
   PostcssPlugin.postcss = true;
